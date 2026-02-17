09eb35117316438fca5b6400cf37e797b1fa0a72
house <house@192.168.1.8>
2026-02-17 18:36:28 -0500

    conexion 1.1

--- conexion.php
+++ conexion.php
@@ -1,16 +1,19 @@
+
 <?php
+$url = getenv("DATABASE_URL");
 
-$host = $_ENV['mysql.railway.internal'];      // mysql.railway.internal
-$user = $_ENV['root'];      // root
-$pass = $_ENV['DlRenYKhIPxKaVNlNlgUOuxRuHldHPzW'];  // contraseña
-$db   = $_ENV['railway'];  // railway
-$port = $_ENV['3306'];      // 3306
+$parts = parse_url($url);
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$host = $parts['mysql.railway.internal'];
+$user = $parts['root'];
+$pass = $parts['DlRenYKhIPxKaVNlNlgUOuxRuHldHPzW'];
+$db   = ltrim($parts['railway'],'/');
+
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error: " . $conn->connect_error);
 }
 
-echo "Conectado a Railway correctamente";
+echo "Conectado correctamente";
 ?>
