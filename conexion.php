df087b5b319c0656eb160370c53de724f7b67610
house <house@192.168.1.8>
2026-02-17 18:49:29 -0500

    conexion 1.2

--- conexion.php
+++ conexion.php
@@ -1,19 +1,27 @@
-
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $url = getenv("DATABASE_URL");
+
+if (!$url) {
+    die("No se encontró DATABASE_URL");
+}
 
 $parts = parse_url($url);
 
-$host = $parts['mysql.railway.internal'];
-$user = $parts['root'];
-$pass = $parts['DlRenYKhIPxKaVNlNlgUOuxRuHldHPzW'];
-$db   = ltrim($parts['railway'],'/');
+$host = $parts['host'];
+$user = $parts['user'];
+$pass = $parts['pass'];
+$db   = ltrim($parts['path'],'/');
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Error: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error);
 }
 
-echo "Conectado correctamente";
+echo "✅ Conectado correctamente a la base de datos";
+
 ?>
