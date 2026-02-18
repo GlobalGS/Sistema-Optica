4cb2db288aa8c1bef45d4321248bf3b22ed970a7
house <house@192.168.1.8>
2026-02-17 19:30:49 -0500

    conexion 1.8

--- conexion.php
+++ conexion.php
@@ -9,16 +9,12 @@ $pass = getenv("MYSQLPASSWORD");
 $db   = getenv("MYSQLDATABASE");
 $port = getenv("MYSQLPORT");
 
-if (!$host || !$user || !$db) {
-    die("Faltan variables de entorno de MySQL");
-}
-
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 
-echo "✅ Conectado correctamente a la base de datos: " . $db;
+echo "✅ Conectado correctamente a la base de datos";
 
 ?>
